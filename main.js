cb4dddbed6d0d14135003f7f3f441312acbb0742
tickie-glitch <74769020+tickie-glitch@users.noreply.github.com>
2024-05-15 17:56:59 +0200

    voilaaaa

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { getPostcardUrl } from "firebase.storage.js";
+import { getPostcardUrl } from "./firebase.storage.js";
 import { getRandomRotation, getRandomBetween } from "./utils";
 import { getAllPostcards, getPostcardPosition, setPostcardPosition } from "./firebase";
 
