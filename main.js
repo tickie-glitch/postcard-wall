c6cfd67445a4aa031fdb6e1c07e30ae5f2345449
tickie-glitch <74769020+tickie-glitch@users.noreply.github.com>
2024-05-16 16:04:30 +0200

    sacaling

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import { getRandomRotation, getRandomBetween } from "./utils";
 import { getAllPostcards, getPostcardPosition, onNewPostcard, setPostcardPosition } from "./firebase";
 
 
-let cardWidth = (3840 / 143.9) * 15.3
-let cardHeight = cardWidth * ( 10.16 /15.3 )
+let cardWidth = ((3840 / 143.9) * 15.3) /2
+let cardHeight = (cardWidth * ( 10.16 /15.3 )) /2
 
 
 async function displayPostcards() {
